acd8f6394042d3ad1879f065e4a3e4b38a732795
mikutas <23391543+mikutas@users.noreply.github.com>
2023-08-08 21:15:59 +0900

    feat: Linux return codes

--- src/errors.rs
+++ src/errors.rs
@@ -19,3 +19,30 @@ impl fmt::Display for Errcode {
         }
     }
 }
+
+use std::process::exit;
+
+// Get the result from a function, and exit the process with the correct error code
+pub fn exit_with_retcode(res: Result<(), Errcode>) {
+    match res {
+        // If it's a success, return 0
+        Ok(_) => {
+            log::debug!("Exit without any error, returning 0");
+            exit(0);
+        },
+
+        // If there's an error, print an error message and return the retcode
+        Err(e) => {
+            let retcode = e.get_retcode();
+            log::error!("Error on exit:\n\t{}\n\tReturning {}", e, retcode);
+            exit(retcode);
+        }
+    }
+}
+
+impl Errcode{
+    // Translate an Errcode::X into a number to return (the Unix way)
+    pub fn get_retcode(&self) -> i32 {
+        1 // Everything != 0 will be treated as an error
+    }
+}
